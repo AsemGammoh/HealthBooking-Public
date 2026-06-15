325c7590a24cafc3e201be7a7661d6f0436971bd
AsemGammoh <103935272+AsemGammoh@users.noreply.github.com>
2026-06-15 11:00:33 +0300

    Update AppointmentsList.vue

--- views/AppointmentsList.vue
+++ views/AppointmentsList.vue
@@ -47,6 +47,7 @@
 </template>
 
 <script>
+const API_BASE_URL = "https://2r4l7cheo4.execute-api.eu-north-1.amazonaws.com/prod";
 export default {
   name: "AppointmentsList",
   data() {
@@ -59,7 +60,7 @@ export default {
   },
   methods: {
     fetchAppointments() {
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments")
+      fetch("https://2r4l7cheo4.execute-api.eu-north-1.amazonaws.com/prod/appointments")
         .then(res => res.json())
         .then(data => {
           const parsed = JSON.parse(data.body);
@@ -74,7 +75,7 @@ export default {
       console.log("appointmentId:", cleanAppointment.appointmentId);
       console.log(" appointmentId (direct):", appointment.appointmentId);
 
-      const url = `https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments/${appointment.appointmentId}`;
+      const url = `https://2r4l7cheo4.execute-api.eu-north-1.amazonaws.com/prod/appointments/${appointment.appointmentId}`;
 
       const payload = { status: newStatus };
 
